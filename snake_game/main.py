7dcc9aaafe984c2b1ca6bf41e1406fa2933318c8
Bruno Marçau <127690155+b01tech@users.noreply.github.com>
2023-06-24 16:04:58 -0300

    fix: fix the body collision bug

--- snake_game/main.py
+++ snake_game/main.py
@@ -3,6 +3,7 @@ import time
 from snake import Snake
 from food import Food
 from score_board import ScoreBoard
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
@@ -17,10 +18,10 @@ food = Food()
 score = ScoreBoard()
 
 screen.listen()
-screen.onkey(key='Up', fun=snake.goup)
-screen.onkey(key='Down', fun=snake.godown)
-screen.onkey(key='Left', fun=snake.goleft)
-screen.onkey(key='Right', fun=snake.goright)
+screen.onkey(key="Up", fun=snake.goup)
+screen.onkey(key="Down", fun=snake.godown)
+screen.onkey(key="Left", fun=snake.goleft)
+screen.onkey(key="Right", fun=snake.goright)
 
 while is_game_on:
     screen.update()
@@ -30,12 +31,17 @@ while is_game_on:
         score.get_point()
         snake.extend()
         food.refresh()
-    if snake.head.xcor() > 280 or snake.head.xcor() < -299 or snake.head.ycor() > 299 or snake.head.ycor() < -280:
+    if (
+        snake.head.xcor() > 280
+        or snake.head.xcor() < -299
+        or snake.head.ycor() > 299
+        or snake.head.ycor() < -280
+    ):
         score.game_over()
         is_game_on = False
 
     for part in snake.body[1:]:
-        if snake.head.distance(part) < 10:
+        if snake.head.distance(part) < 5:
             score.game_over()
             is_game_on = False
 
